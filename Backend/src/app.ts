5e4aad458170d576e3a01db0548561d04d2eed12
pabitra <89703515+appu900@users.noreply.github.com>
2024-04-21 19:20:41 +0530

    added global error handler

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,17 @@
-import express from 'express';
+import express from "express";
 const app = express();
+import { Request, Response, NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorHandler from "./middlewares/global-error-handler";
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+    const error = createHttpError(404, "This route does not exist");
+    throw error;
+    res.send("Hello World");
 });
 
+// ** global error handler //
+app.use(globalErrorHandler);
 
 export default app;
--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -1,8 +1,9 @@
-import {config as dconf } from 'dotenv';
+import { config as dconf } from "dotenv";
 dconf();
 
 const _config = {
-    port : process.env.PORT || 3000
-}
+    port: process.env.PORT || 3000,
+    env: process.env.NODE_ENV,
+};
 
 export const config = Object.freeze(_config);
